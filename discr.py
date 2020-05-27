656bf415c439574932ade27d2c3337803ecd8c26
Andy <47635700+AndyDroid10X@users.noreply.github.com>
2020-05-27 23:58:00 +0500

    Add files via upload

---
+++ discr.py
@@ -0,0 +1,18 @@
+# нахождение корней квадратоного уравнения
+import math
+
+
+def discriminant(a, b, c):
+    d = b ** 2 - (4 * a * c)
+    if d > 0:
+        x1 = (-b + math.sqrt(d)) / 2 * a
+        x2 = (-b - math.sqrt(d)) / 2 * a
+        print("X1 = " + str(x1) + "\nX2 = " + str(x2))
+    elif d == 0:
+        x = -b / (2 * a)
+        print("У уравнения только один корень: " + str(x))
+    elif d < 0:
+        print("У уравнения нет решения")
+
+
+discriminant(float(input("Введите A:")), float(input("Введите B:")), float(input("Введите C:")))
