e1c9e3cac59b995f88417d31c3ec91c34d5b7025
Andy <47635700+AndyDroid10X@users.noreply.github.com>
2020-07-16 22:42:47 +0500

    Hangman game

---
+++ hangman.py
@@ -0,0 +1,31 @@
+import random
+
+words = ['snake', 'tardis', 'dalek', 'gun']
+n = len(words)
+word = list(words[random.randint(0, n-1)])
+#print(word)
+count = len(word)
+tab = list('_'*count)
+print(tab)
+inp = input()[0]
+tries = 10
+
+while tries > 0:
+    if inp in word:
+        print("Right!")
+        tab.insert(word.index(inp), inp)
+        tab.pop(word.index(inp) + 1)
+        print(tab)
+        if tab != word:
+            inp = input()
+    elif inp not in word:
+        print("Wrong!")
+        tries = tries - 1
+        if tries == 0:
+            print("Exterminate! *You're dead*")
+            break
+        print(tab)
+        inp = input()
+    if tab == word:
+        print("Geronimo! You won)")
+        break
